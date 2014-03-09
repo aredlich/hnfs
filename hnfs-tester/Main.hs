9bb2e633d77edf8ec9cf11f75a673f4713063a21
Arne Redlich <arne.redlich@googlemail.com>
2014-03-09 23:18:14 +0100

    First *working* version of hnfs-tester
    Signed-off-by: Arne Redlich <arne.redlich@googlemail.com>

--- hnfs-tester/Main.hs
+++ hnfs-tester/Main.hs
@@ -9,8 +9,104 @@
 
 module Main where
 
+import Control.Concurrent.MVar
+
+import Data.IORef
+import Data.Monoid
+
+import qualified GHC.Event as Ev
+
 import qualified System.Console.CmdArgs as CA
 import qualified System.Nfs as Nfs
+
+import qualified Test.Tasty.HUnit as HU
+
+nfs_to_ev_event :: Nfs.Event -> Ev.Event
+nfs_to_ev_event ev
+  | ev == Nfs.eventRead `mappend` Nfs.eventWrite = Ev.evtRead `mappend` Ev.evtWrite
+  | ev == Nfs.eventRead = Ev.evtRead
+  | ev == Nfs.eventWrite = Ev.evtWrite
+  | otherwise = undefined
+
+ev_to_nfs_event :: Ev.Event -> Nfs.Event
+ev_to_nfs_event ev
+  | ev == Ev.evtRead `mappend` Ev.evtWrite = Nfs.eventRead `mappend` Nfs.eventWrite
+  | ev == Ev.evtRead = Nfs.eventRead
+  | ev == Ev.evtWrite = Nfs.eventWrite
+  | otherwise = undefined
+
+-- We need to constantly re-register the context's fd as libnfs closes / reopens
+-- it.
+-- TODO: get rid of the IORef
+ev_callback :: Nfs.Context -> Ev.EventManager -> IORef (Maybe Ev.FdKey) -> Ev.FdKey -> Ev.Event -> IO ()
+ev_callback ctx mgr ref fdkey ev = do
+  Ev.unregisterFd mgr fdkey
+  Nfs.service ctx $ ev_to_nfs_event ev
+  register_fd ctx mgr ref
+
+register_fd :: Nfs.Context -> Ev.EventManager -> IORef (Maybe Ev.FdKey) -> IO ()
+register_fd ctx mgr ref = do
+  fd <- Nfs.getFd ctx
+  evts <- Nfs.whichEvents ctx
+  key <- Ev.registerFd mgr (ev_callback ctx mgr ref) fd $ nfs_to_ev_event evts
+  writeIORef ref $ Just key
+
+sync_wrap :: Show a => Nfs.Context ->
+             (Nfs.Callback a -> IO (Either String ())) ->
+             IO (Either String a)
+sync_wrap ctx async_action = do
+  mmgr <- Ev.getSystemEventManager
+  case mmgr of
+    Nothing -> return $ Left "failed to get system event manager"
+    Just mgr -> do
+      mv <- newEmptyMVar
+      ret <- async_action $ \ret' -> do
+        putMVar mv ret'
+      case ret of
+        Left s -> return $ Left $ "failed to invoke async action: " ++ s
+        Right () -> do
+          ref <- newIORef Nothing
+          register_fd ctx mgr ref
+          ret'' <- takeMVar mv
+          mkey <- readIORef ref
+          case mkey of
+            Nothing -> return $ Left "failed to get fdkey to unregister"
+            Just key -> do
+              Ev.unregisterFd mgr key
+              return ret''
+
+-- TODO: use Either + IO monad transformer stack
+mount_and_list :: Nfs.ServerAddress -> Nfs.ExportName -> FilePath -> IO ()
+mount_and_list srv xprt path = do
+  ctx <- Nfs.initContext
+  ret <- sync_wrap ctx $ Nfs.mountAsync ctx srv xprt
+  case ret of
+    Left s -> HU.assertFailure $ "failed to mount " ++ srv ++ ":" ++ xprt ++ ": " ++ s
+    Right _ -> do
+      ret' <- sync_wrap ctx $ Nfs.openDirAsync ctx path
+      case ret' of
+        Left s -> HU.assertFailure $ "failed to open dir \"/\": " ++ s
+        Right dir -> print_entries ctx dir
+    where
+      print_entries :: Nfs.Context -> Nfs.Dir -> IO ()
+      print_entries ctx' dir' = do
+        maybe_entry <- Nfs.readDir ctx' dir'
+        case maybe_entry of
+          Nothing -> return ()
+          Just e -> do
+            putStrLn $
+              "{ name = " ++ (Nfs.direntName e) ++
+              ", type = " ++ (show $ Nfs.direntFType3 e) ++
+              ", size = " ++ (show $ Nfs.direntSize e) ++
+              ", mode = " ++ (show $ Nfs.direntMode e) ++
+              ", uid = " ++ (show $ Nfs.direntUid e) ++
+              ", gid = " ++ (show $ Nfs.direntGid e) ++
+              ", atime = " ++ (show $ Nfs.direntATime e) ++
+              ", ctime = " ++ (show $ Nfs.direntCTime e) ++
+              ", mtime = " ++ (show $ Nfs.direntMTime e) ++
+              ", inode = " ++ (show $ Nfs.direntInode e) ++
+              "}"
+            print_entries ctx' dir'
 
 data Args = Args { server :: Nfs.ServerAddress
                  , export :: Nfs.ExportName
@@ -42,8 +138,7 @@ args = CA.cmdArgsMode $  Args { server = CA.def CA.&= CA.help "NFS server addres
 main :: IO ()
 main = do
   a <- CA.cmdArgsRun args
-  putStrLn $ "Using nfs://" ++ (server a) ++ "/" ++ (export a)
-
+  mount_and_list (server a) (export a) "/"
 
 -- Local Variables: **
 -- mode: haskell **
